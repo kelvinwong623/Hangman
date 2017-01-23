a10982b3368403cfbb11fa5dee005bb8c355486b
leafStudios <kelvinwong623@gmail.com>
2017-01-23 15:53:29 -0800

    update to progress

--- app/src/main/java/leafstudios/hangman/gameplay.java
+++ app/src/main/java/leafstudios/hangman/gameplay.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class gameplay extends AppCompatActivity {
 
@@ -34,8 +37,11 @@ public class gameplay extends AppCompatActivity {
     private Button X_button;
     private Button Y_button;
     private Button Z_button;
-    private Letter[] word;
+    private Button Hint_button;
+    private ImageView GallowsView;
+    private ArrayList<Letter> word;
     private boolean[] usedChar;
+    private int misses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +54,6 @@ public class gameplay extends AppCompatActivity {
         }
 
         Intent intent = getIntent();
-
         A_button = (Button) findViewById(R.id.A_button);
         B_button = (Button) findViewById(R.id.B_button);
         C_button = (Button) findViewById(R.id.C_button);
@@ -75,12 +80,23 @@ public class gameplay extends AppCompatActivity {
         X_button = (Button) findViewById(R.id.X_button);
         Y_button = (Button) findViewById(R.id.Y_button);
         Z_button = (Button) findViewById(R.id.Z_button);
+        Hint_button = (Button) findViewById(R.id.Hint_Button);
+        GallowsView = (ImageView) findViewById(R.id.GallowsView);
+
+        Hint_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         A_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 usedChar[0] = true;
                 checkLetter('A');
+                A_button.setVisibility(View.GONE);
+                A_button.setOnClickListener(null);
             }
         });
 
@@ -89,6 +105,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[1] = true;
                 checkLetter('B');
+                B_button.setVisibility(View.GONE);
+                B_button.setOnClickListener(null);
             }
         });
 
@@ -97,6 +115,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[2] = true;
                 checkLetter('C');
+                C_button.setVisibility(View.GONE);
+                C_button.setOnClickListener(null);
             }
         });
 
@@ -105,6 +125,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[3] = true;
                 checkLetter('D');
+                D_button.setVisibility(View.GONE);
+                D_button.setOnClickListener(null);
             }
         });
 
@@ -113,6 +135,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[4] = true;
                 checkLetter('E');
+                E_button.setVisibility(View.GONE);
+                E_button.setOnClickListener(null);
             }
         });
 
@@ -121,6 +145,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[5] = true;
                 checkLetter('F');
+                F_button.setVisibility(View.GONE);
+                F_button.setOnClickListener(null);
             }
         });
 
@@ -129,6 +155,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[6] = true;
                 checkLetter('G');
+                G_button.setVisibility(View.GONE);
+                G_button.setOnClickListener(null);
             }
         });
 
@@ -137,6 +165,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[7] = true;
                 checkLetter('H');
+                H_button.setVisibility(View.GONE);
+                H_button.setOnClickListener(null);
             }
         });
 
@@ -145,6 +175,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[8] = true;
                 checkLetter('I');
+                I_button.setVisibility(View.GONE);
+                I_button.setOnClickListener(null);
             }
         });
 
@@ -153,6 +185,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[9] = true;
                 checkLetter('J');
+                J_button.setVisibility(View.GONE);
+                J_button.setOnClickListener(null);
             }
         });
 
@@ -161,6 +195,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[10] = true;
                 checkLetter('K');
+                K_button.setVisibility(View.GONE);
+                K_button.setOnClickListener(null);
             }
 
         });
@@ -170,6 +206,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[11] = true;
                 checkLetter('L');
+                L_button.setVisibility(View.GONE);
+                L_button.setOnClickListener(null);
             }
         });
 
@@ -178,6 +216,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[12] = true;
                 checkLetter('M');
+                M_button.setVisibility(View.GONE);
+                M_button.setOnClickListener(null);
             }
         });
 
@@ -186,6 +226,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[13] = true;
                 checkLetter('N');
+                N_button.setVisibility(View.GONE);
+                N_button.setOnClickListener(null);
             }
         });
 
@@ -194,6 +236,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[14] = true;
                 checkLetter('O');
+                O_button.setVisibility(View.GONE);
+                O_button.setOnClickListener(null);
             }
         });
 
@@ -202,6 +246,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[15] = true;
                 checkLetter('P');
+                P_button.setVisibility(View.GONE);
+                P_button.setOnClickListener(null);
             }
         });
 
@@ -210,6 +256,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[16] = true;
                 checkLetter('Q');
+                Q_button.setVisibility(View.GONE);
+                Q_button.setOnClickListener(null);
             }
         });
 
@@ -218,6 +266,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[17] = true;
                 checkLetter('R');
+                R_button.setVisibility(View.GONE);
+                R_button.setOnClickListener(null);
             }
         });
 
@@ -226,6 +276,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[18] = true;
                 checkLetter('S');
+                S_button.setVisibility(View.GONE);
+                S_button.setOnClickListener(null);
             }
         });
 
@@ -234,6 +286,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[19] = true;
                 checkLetter('T');
+                T_button.setVisibility(View.GONE);
+                T_button.setOnClickListener(null);
             }
         });
 
@@ -242,6 +296,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[20] = true;
                 checkLetter('U');
+                U_button.setVisibility(View.GONE);
+                U_button.setOnClickListener(null);
             }
         });
 
@@ -250,6 +306,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[21] = true;
                 checkLetter('V');
+                V_button.setVisibility(View.GONE);
+                V_button.setOnClickListener(null);
             }
         });
 
@@ -258,6 +316,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[22] = true;
                 checkLetter('W');
+                W_button.setVisibility(View.GONE);
+                W_button.setOnClickListener(null);
             }
         });
 
@@ -266,6 +326,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[23] = true;
                 checkLetter('X');
+                X_button.setVisibility(View.GONE);
+                X_button.setOnClickListener(null);
             }
         });
 
@@ -274,6 +336,8 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[24] = true;
                 checkLetter('Y');
+                Y_button.setVisibility(View.GONE);
+                Y_button.setOnClickListener(null);
             }
         });
 
@@ -282,13 +346,60 @@ public class gameplay extends AppCompatActivity {
             public void onClick(View view) {
                 usedChar[25] = true;
                 checkLetter('Z');
+                Z_button.setVisibility(View.GONE);
+                Z_button.setOnClickListener(null);
             }
         });
 
-
+        gameStart();
     }
 
     public void checkLetter(char test){
 
+    }
+
+    public void strike(){
+        misses = misses + 1;
+        updateGallows(misses);
+    }
+
+    private void getWord(ArrayList<Letter> word){
+
+    }
+
+    private void gameStart(){
+        misses = 0;
+        ArrayList<Letter> word = new ArrayList<Letter>();
+        getWord(word);
+    }
+
+    private void checkWin(){
+
+    }
+
+    private void updateGallows(int x){
+        switch(x){
+            case 0:
+                GallowsView.setImageResource(R.drawable.hang0);
+                break;
+            case 1:
+                GallowsView.setImageResource(R.drawable.hang1);
+                break;
+            case 2:
+                GallowsView.setImageResource(R.drawable.hang2);
+                break;
+            case 3:
+                GallowsView.setImageResource(R.drawable.hang3);
+                break;
+            case 4:
+                GallowsView.setImageResource(R.drawable.hang4);
+                break;
+            case 5:
+                GallowsView.setImageResource(R.drawable.hang5);
+                break;
+            case 6:
+                GallowsView.setImageResource(R.drawable.hang6);
+                break;
+        }
     }
 }
